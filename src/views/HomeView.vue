7bb859c6ec893fce4ff7087b9a08f6e6264f9ecb
Demitri Grant <demitri.grant@gmail.com>
2022-04-04 12:18:45 -0500

    updates

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script>
+import NewsList from '@/components/NewsList.vue'
+
 export default {
     data() {
         return {
-            message: "Hello World! Welcome to VueJS"
+            message: "Hello World! Welcome to News Room."
         };
     },
+    components:{NewsList}
 }
+
 </script>
 
 <template>
@@ -13,6 +17,7 @@ export default {
       <div class="text-center">
         <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
         <h1>{{ message }}</h1>
+        <NewsList/>
       </div>
     </div>
 </template>
